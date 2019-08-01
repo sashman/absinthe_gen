5239fcffb608e4c861d58588d41c03b60101e9a5
sashman <sashman90@gmail.com>
2019-08-01 21:46:23 +0100

    Adding resolver

--- lib/mix/absinthe.gen.scaffold.ex
+++ lib/mix/absinthe.gen.scaffold.ex
@@ -23,12 +23,15 @@ defmodule Mix.Tasks.Absinthe.Gen.Scaffold do
     }
 
     types_contents = RenderTemplate.render_types(ctx, schema)
-    types_schema = RenderTemplate.render_schema(ctx, schema)
+    schema_contents = RenderTemplate.render_schema(ctx, schema)
+    resolver_contents = RenderTemplate.render_resolver(ctx, schema)
 
-    OutputFiles.write_files([
+    [
       {Path.join(["lib", "#{parent_app}_web", "schema", "#{context}_types.ex"]), types_contents},
-      {Path.join(["lib", "#{parent_app}_web", "schema.ex"]), types_schema}
-    ])
+      {Path.join(["lib", "#{parent_app}_web", "schema.ex"]), schema_contents},
+      {Path.join(["lib", "#{parent_app}_web", "resolvers", "#{context}.ex"]), resolver_contents}
+    ]
+    |> OutputFiles.write_files()
   end
 
   defp parse_attrs(attrs) when is_list(attrs) do
--- lib/render_template.ex
+++ lib/render_template.ex
@@ -1,21 +1,27 @@
 defmodule AbsintheGen.RenderTemplate do
   @template_directory "templates"
 
-  def render_types(context = %{web_module: _}, schema = %{alias: _, name: _, attrs: attrs})
-      when is_map(attrs),
-      do: render("types.eex", context: context, schema: schema)
+  def render_types(context, schema),
+    do: render("types.eex", context, schema)
 
-  def render_schema(context = %{web_module: _}, schema = %{alias: _, name: _, attrs: attrs})
-      when is_map(attrs),
-      do: render("schema.eex", context: context, schema: schema)
+  def render_schema(context, schema),
+    do: render("schema.eex", context, schema)
 
-  def render(template_name, bindings) do
+  def render_resolver(context, schema),
+    do: render("resolver.eex", context, schema)
+
+  def render(
+        template_name,
+        %{web_module: _} = context,
+        %{alias: _, name: _, attrs: attrs} = schema
+      )
+      when is_map(attrs) do
     file_dir =
       __ENV__.file
       |> Path.dirname()
 
     [file_dir, "..", @template_directory, template_name]
     |> Path.join()
-    |> EEx.eval_file(bindings, trim: true)
+    |> EEx.eval_file([context: context, schema: schema], trim: true)
   end
 end
