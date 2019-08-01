84208db66fddbe9c6585b36fe1034fe02f9fc6ee
sashman <sashman90@gmail.com>
2019-08-01 22:13:51 +0100

    Updating mix for publishing

--- mix.exs
+++ mix.exs
@@ -7,8 +7,13 @@ defmodule AbsintheGen.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      dialyzer: [plt_add_apps: [:mix]],
+      build_embedded: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
-      dialyzer: [plt_add_apps: [:mix]]
+      name: "AbsintheGen",
+      source_url: "https://github.com/sashman/absinthe_gen"
     ]
   end
 
@@ -19,12 +24,22 @@ defmodule AbsintheGen.MixProject do
     ]
   end
 
+  defp description() do
+    "Set of tasks to generate boiler plate files for Elixir Absinthe."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/sashman/absinthe_gen"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
