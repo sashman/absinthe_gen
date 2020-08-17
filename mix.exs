6fcc24198d7c1df34af40f5fb3a26c2d491fdba1
sash stasyk <sashman90@gmail.com>
2020-08-17 13:57:52 +0100

    Bump patch version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AbsintheGen.MixProject do
   def project do
     [
       app: :absinthe_gen,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       dialyzer: [plt_add_apps: [:mix]],
